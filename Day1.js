ea44029243d181771e9b6ee352f87df68fb4f426
Pranav <pranavmendi@gmail.com>
2020-12-05 01:38:38 +0530

    Added Day1.js

---
+++ Day1.js
@@ -0,0 +1,21 @@
+// There are 3 data types in Javascript:
+// 1. Numbers
+// 2. String
+// 3. Boolean
+// I'll be testing js code in developer tools/sources/snippets on my chrome browser.
+// (Clear cache for new programs)
+
+
+alert("Hello world!"); // gives pop up of hello world
+
+prompt("What is your name?"); //gives pop up with input box
+
+// Variables:
+
+var myName = "Pranav Mendiratta"; //new variable
+myName = "Pranav"; //updating existing var
+
+//Code snippets-
+
+var yourName = prompt("What's your name?");
+alert("Welcome to my website " + yourName);
