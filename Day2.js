da0ec569b916876d5e66c3f85166e938a4ba1315
Pranav <pranavmendi@gmail.com>
2020-12-05 05:10:32 +0530

    Added Day2.js

---
+++ Day2.js
@@ -0,0 +1,16 @@
+//Arithmetic operations-
+// multiplication *
+// addition +
+// subtraction -
+// modulus %
+// division /
+
+// Presedence of operators exists
+
+
+// Increment x++  and  x+=2
+// Decrement x--  and  x-=2
+
+
+// Functions/ Methods-
+
