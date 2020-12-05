7f15c3fdd42f6aa477df75b24da753bf30a6f70b
Pranav <pranavmendi@gmail.com>
2020-12-06 03:11:25 +0530

    Added Day5.js

---
+++ Day5.js
@@ -0,0 +1,28 @@
+//Arrays
+
+var len=arr.length
+var sorted= arr.sort() //by default this sort compares elements as strings. For numeric sort, we can provide a compare function.
+arr.reverse()
+
+
+//Adding elements at the end
+arr.push(12)
+
+//Removing elements from the end
+var x = arr.pop()
+
+//Adding elements at the start
+arr.unshift(12)
+
+//Removing elements from the start
+var x = arr.shift()
+
+//deleting elements
+delete arr[0]
+
+// The Difference Between Arrays and Objects
+
+// In JavaScript, arrays use numbered indexes.  
+// In JavaScript, objects use named indexes.
+
+//Array methods
