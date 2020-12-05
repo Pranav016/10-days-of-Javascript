0926d72592cc27d4d99df8d0c84f7edf2eb9bc0c
Pranav <pranavmendi@gmail.com>
2020-12-06 01:02:48 +0530

    Added Day4.js

---
+++ Day4.js
@@ -0,0 +1,33 @@
+// Control statements
+
+// is equal to: == (checks value matches)
+// is equal: === (also check if data types match)
+// not equal: !==
+// less than: >
+// greater than: <
+// less than equal to: <=
+// greater than equal to: >=
+
+
+//Combinators
+
+//AND- &&
+//OR- ||
+//NOT- !
+
+
+
+//Comparators
+
+// if ()
+if( ){
+
+}
+// else if()
+else if( ){
+
+}
+// else
+else{
+
+}
