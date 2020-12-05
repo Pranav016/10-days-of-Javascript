b3a05aa62359a93b230b6cce46b855a337b42365
Pranav <pranavmendi@gmail.com>
2020-12-05 21:26:59 +0530

    Added Day3.js

---
+++ Day3.js
@@ -0,0 +1,23 @@
+// Types of loops-
+// for 
+// while
+
+
+// Types of for loops-
+// 1. for 
+for (i=0;i<name;i++){  }
+// 2. for/in
+for (x in person){  }
+// 3. for/of 
+for (x of person){  }
+
+
+// Types of while loops-
+// 1. while 
+while (condition) {
+    
+}
+// 2. do/while 
+do{
+
+}while(i<90);
