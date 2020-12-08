0812ca151d48ca3c3b0d8f3ea86a5c38702e6d6b
Pranav <pranavmendi@gmail.com>
2020-12-08 07:19:20 +0530

    Updated DOM5.js

---
+++ DOM5.js
@@ -0,0 +1,8 @@
+// Code snippets
+
+element.querySelector("button").classList.add("invisible"); //instead of adding CSS, we apply a class that has associated CSS in style.css file
+element.querySelector("button").classList.remove("invisible");
+element.querySelector("button").classList.toggle("invisible");
+
+
+element.querySelector("a").setAttribute("href","google.com");
