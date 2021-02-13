1c28a8a71019b0bb5f7e4a47d21ff95a77abd868
Pranav <pranavmendi@gmail.com>
2021-02-13 23:47:35 +0530

    Update ArrowFunctions&FunctionContext.js and Classes&ModuleExports.js

--- ES6/ArrowFunctions&FunctionContext.js
+++ ES6/ArrowFunctions&FunctionContext.js
@@ -5,13 +5,13 @@ var fun = () => {
 }
 
 // context of functions and variables-
-sayHello(); //this will execute fine
+sayHello(); //this will execute fine since not stored in a variable, regular function call
 
 function sayHello(){
     console.log("Hello");
 }
 
-sayBye(); //this gives error
+sayBye(); //this gives error, stored in a variable
 
 var sayBye = () =>{
     console.log("Bye");
@@ -20,3 +20,30 @@ var sayBye = () =>{
 console.log(sayHi) //this gives 'undefined'
 
 var sayHi = "Hi";
+
+
+
+// ARROW FUNCTIONS
+
+// Converting it to an arrow function
+// const Pranav = ()=>{
+//     console.log("This is the best person ever")
+// }
+// Pranav();
+
+
+// One liners do not require braces/return
+// one line will automatically return
+// const greet = () =>  "Good Morning";
+
+
+// Arrow functions in case we want to return objects
+// const greet = () =>  ({name: "Pranav"});
+
+// Single parameters do not need parenthesis 
+// but you will have to put parenthesis if there are multiple paramteres
+const greet = name =>  "Good Morning " + name;
+
+const greet = (name,ending) =>  "Good Morning " + name + ending;
+
+console.log(greet('Pranav'))
--- ES6/Classes&ModuleExports.js
+++ ES6/Classes&ModuleExports.js
@@ -1,11 +1,11 @@
-class User{
-    constructor(name, email){
-        this.name = name;
-        this.email = email;
-    }
-    getInfo(){
-        console.log(`Name is ${this.name} and email is ${this.email}`);
-    }
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  getInfo() {
+    console.log(`Name is ${this.name} and email is ${this.email}`);
+  }
 }
 
 module.exports = User;
